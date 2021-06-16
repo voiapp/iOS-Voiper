bc5f677068df997e18ae03c2dfeca930f489bdb2
David Jangdal <djangdal@gmail.com>
2021-06-16 22:50:23 +0200

    removed force cast from presenter

--- Sources/Voiper/Presenter.swift
+++ Sources/Voiper/Presenter.swift
@@ -1,20 +1,20 @@
 
 public typealias Presenter = PresenterClass & PresenterType & Injectable
 open class PresenterClass: PresenterProtocol {
-    public weak var _viewDelegate: ViewControllerProtocol?
-    public var _interactor: InteractorProtocol?
-    public var _router: RouterProtocol?
+    private weak var viewDelegate: ViewControllerProtocol?
+    private var interactor: InteractorProtocol?
+    private var router: RouterProtocol?
     
     public func set(viewDelegate: ViewControllerProtocol) {
-        self._viewDelegate = viewDelegate
+        self.viewDelegate = viewDelegate
     }
     
     public func set(interactor: InteractorProtocol) {
-        self._interactor = interactor
+        self.interactor = interactor
     }
     
     public func set(router: RouterProtocol) {
-        self._router = router
+        self.router = router
     }
     
     public init() {
@@ -27,34 +27,25 @@ public protocol PresenterProtocol {
     func set(router: RouterProtocol)
 }
 
-public extension PresenterProtocol {
-    func set(viewDelegate: ViewControllerProtocol) {}
-    func set(interactor: InteractorProtocol) {}
-    func set(router: RouterProtocol) {}
-}
-
 public protocol PresenterType {
     associatedtype ViewDelegateType
     associatedtype InteractorType
     associatedtype RouterType
-    var _viewDelegate: ViewControllerProtocol? {get}
-    var _interactor: InteractorProtocol? {get}
-    var _router: RouterProtocol? {get}
-    var viewDelegate: ViewDelegateType {get}
-    var interactor: InteractorType {get}
-    var router: RouterType {get}
+    var viewDelegate: ViewDelegateType? {get}
+    var interactor: InteractorType? {get}
+    var router: RouterType? {get}
 }
 
 public extension PresenterType {
-    var viewDelegate: ViewDelegateType {
-        return (_viewDelegate as! ViewDelegateType)
+    var viewDelegate: ViewDelegateType? {
+        return viewDelegate
     }
-    
-    var interactor: InteractorType {
-        return (_interactor as! InteractorType)
+
+    var interactor: InteractorType? {
+        return interactor
     }
-    
-    var router: RouterType {
-        return (_router as! RouterType)
+
+    var router: RouterType? {
+        return router
     }
 }
