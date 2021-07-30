48fca9d926a92ef3598bf3328ab16a16931380bd
ekaterina-dv <ekaterina.dvuzhilova@voiapp.io>
2021-07-30 16:46:37 +0200

    Minor code improvements

--- Sources/Voiper/VOIPER.swift
+++ Sources/Voiper/VOIPER.swift
@@ -114,12 +114,28 @@ extension Organiser where Presenter.Configuration == Void, Interactor.Configurat
 
 extension Organiser where Presenter: Embeddable, ViewController: ViewControllerType {
     public static func createAsChildModule(presenterConfiguration: Presenter.Configuration, interactorConfiguration: Interactor.Configuration, routerConfiguration: Router.Configuration) -> (UIViewController, Presenter.ParentControllable) {
-        let vc = wireUpModule(presenter: Presenter(configuration: presenterConfiguration),
-                              interactor: Interactor(configuration: interactorConfiguration),
-                              router: Router(configuration: routerConfiguration))
-        guard let parentControllable = vc.presenter as? Presenter.ParentControllable else {
+        let childViewController = wireUpModule(presenter: Presenter(configuration: presenterConfiguration),
+                                               interactor: Interactor(configuration: interactorConfiguration),
+                                               router: Router(configuration: routerConfiguration))
+        guard let parentControllable = childViewController.presenter as? Presenter.ParentControllable else {
+            // TODO: it should be fixed, we should think about how to handle it in the best way
+            // make optional or throw error or smth else?
             fatalError("")
         }
-        return (vc, parentControllable)
+        return (childViewController, parentControllable)
+    }
+}
+
+extension Organiser where Presenter: Embeddable, ViewController: ViewControllerType, Presenter.Configuration == Void, Interactor.Configuration == Void, Router.Configuration == Void {
+    public static func createAsChildModule(presenterConfiguration: Presenter.Configuration, interactorConfiguration: Interactor.Configuration, routerConfiguration: Router.Configuration) -> (UIViewController, Presenter.ParentControllable) {
+        let childViewController = wireUpModule(presenter: Presenter(configuration: ()),
+                                               interactor: Interactor(configuration: ()),
+                                               router: Router(configuration: ()))
+        guard let parentControllable = childViewController.presenter as? Presenter.ParentControllable else {
+            // TODO: it should be fixed, we should think about how to handle it in the best way
+            // make optional or throw error or smth else?
+            fatalError("")
+        }
+        return (childViewController, parentControllable)
     }
 }
